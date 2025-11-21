a38001e59da37df4920455d6510fd77174b005e5
ArielMyersWcupa <AM1044820@wcupa.edu>
2025-11-20 21:55:39 -0500

    Create Binop.java

---
+++ Binop.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+/**
+ * Code Author: Ariel Myers
+ * Purpose: A base class for binary arithmetic operations
+ */
+public abstract class Binop extends Op {
+     public abstract double eval(double left, double right);
+}
