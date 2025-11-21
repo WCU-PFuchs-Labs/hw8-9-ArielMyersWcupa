7bf31e3b582c462d32a40de532d875cd49d0290f
ArielMyersWcupa <AM1044820@wcupa.edu>
2025-11-20 22:02:39 -0500

    Create Unop.java

---
+++ Unop.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+/**
+ * Code Author: Ariel Myers
+ * Purpose: A binary operator for Addition
+ */
+ public abstract class Unop extends Op {
+     public abstract double eval(double[] values);
+ }
